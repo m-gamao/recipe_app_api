a970c39ba636ea967c0094f1b88bf09fa5e2a353
gigi <mmgamao1@gmail.com>
2020-08-13 10:05:45 -0700

    edited user validations in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     validates :name, presence: true
-    EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    EMAIL_FORMAT_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, format: { with: EMAIL_FORMAT }, uniqueness: true
+  
+    before_save { self.email = email.downcase }
   end
