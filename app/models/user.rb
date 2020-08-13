97dfb22fab35cd093fbe27cd3ce0d7d97b3fddc2
gigi <mmgamao1@gmail.com>
2020-08-13 10:04:37 -0700

    added user validations in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :name, presence: true
     EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, format: { with: EMAIL_FORMAT }
+    validates :email, presence: true, format: { with: EMAIL_FORMAT }, uniqueness: true
   end
