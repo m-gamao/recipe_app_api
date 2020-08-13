91702f4b917d93b1e9dcbc8966b5a02f181f1db1
gigi <mmgamao1@gmail.com>
2020-08-13 10:03:29 -0700

    added users route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :categories  #, only: [:index, :create]
       resources :sessions, only: [:create]
       resources :meal_plans
+      resources :users
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
